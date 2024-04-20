b9c31fbd2f07d5fe7b54fb94417e70269d8ad565
kabliax <103865881+kabliax@users.noreply.github.com>
2024-04-19 20:25:50 -0500

    Completed Activity 8

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -13,12 +13,12 @@ const StyledHeader = styled.header`
   align-items: center;
   justify-content: space-between;
   height: 50px;
-  background: ${props => props.theme.header.backgroundColor};
+  background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
 `    
 const StyledLink = styled(Link)`
   font-size: var(--font-sm);
   text-decoration: none;
-  color: ${props => props.theme.header.color};
+  color: ${({ theme }) => theme.variants.header.primary.color};
 `
 const Header = ({ siteTitle }) => (
   <Outer>
@@ -30,7 +30,7 @@ const Header = ({ siteTitle }) => (
       </H1>
     </Section>
     <Section width={1/12}>
-      <IconButton icon={<Search />} />
+      <IconButton icon={<Search />} variants='contrast' />
     </Section>
   </Outer>
 )
